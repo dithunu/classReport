00aa7d4f97084fc2cfe09ef67ab4708f806554de
dithunu <n.manomanjari@gmail.com>
2024-07-30 12:00:36 +0530

    Added Comments In Main.c

--- main.c
+++ main.c
@@ -1,10 +1,16 @@
+/*
+	This is the main file of the systme
+	Author: Dithunu
+	Date: 30 - JULY - 2024
+
+*/
+
+
 #include <stdio.h>
+#include <stdlib.h>
 
 int main(){
-
     printf("Hello I am Dithunu");
 
-
-
     return 0;
-}
+}// end of the main function
